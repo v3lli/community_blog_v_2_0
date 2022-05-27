d0553a00a4ba1185e508c7e7fff027a65ebc1581
v3lli <emekaveli@gmail.com>
2022-05-27 20:26:33 +0000

    we go again

--- config/Database.php
+++ config/Database.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
 class Database
 {
-    public $host = getenv("DB_HOST");//"communityblog-server.mysql.database.azure.com:3306";
-    public $db_name = getenv("DB_DATABASE");// "rviii";
-    public $username = getenv("DB_USERNAME"); //"trfnqkctdj";
-    public $password = getenv("DB_PASSWORD");//"6MD5C8YDF8PLBW3V$";
-    public $conn;
+    private $host = "communityblog-server.mysql.database.azure.com";
+    private $db_name = "rviii";
+    private $username = "trfnqkctdj@communityblog-server.mysql.database.azure.com";
+    private $password = "6MD5C8YDF8PLBW3V$";
+    private $conn;
 
     //connect to database
     public function connect() {
